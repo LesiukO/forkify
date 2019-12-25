6b770d493a161ab04c2b52d1e20b69bd198e7d41
LesiukO <lesuksasha@gmail.com>
2019-12-25 22:01:02 +0200

    first API call

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,19 @@
 // Global app controller
-import x from './test'
-console.log('sldkjf')
+
+import axios from 'axios'
+
+async function getResults(query) {
+    try {
+        const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`)
+        const recipes = res.data.recipes
+        console.log(recipes)
+    }
+    catch (error) {
+        alert(error)
+    }
+}
+getResults('lobster')
+
+
+// const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+// const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
