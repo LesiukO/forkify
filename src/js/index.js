b3b935efaaf559a2c4effa706c282cfb997f258f
LesiukO <lesuksasha@gmail.com>
2019-12-29 20:37:27 +0200

    Recipe View Part-2

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@
 import Search from './models/Search'
 import Recipe from './models/Recipe'
 import * as searchView from './views/searchView'
+import * as recipeView from './views/recipeView'
 import { elements, renderLoader, clearLoader } from './views/base'
 
 // // Global state of the app
@@ -19,7 +20,6 @@ const state = {}
 const controlSearch = async () => {
     // 1. Get query from view
     const query = searchView.getInput()
-    // const query = 'pizza' // DELETE
 
     if (query) {
         // 2. New search object and add to state
@@ -70,6 +70,11 @@ const controlRecipe = async () => {
 
     if (id) {
         // Prepare UI for changes
+        recipeView.clearRecipe()
+        renderLoader(elements.recipe)
+
+        // Highlight selected search item
+        if (state.search) searchView.highlightSelected(id)
 
         // Create new recipe object
         state.recipe = new Recipe(id)
@@ -84,7 +89,8 @@ const controlRecipe = async () => {
             state.recipe.calcServings()
 
             // Render recipe
-            console.log(state.recipe)
+            clearLoader()
+            recipeView.renderRecipe(state.recipe)
         } catch (err) {
             alert('Error processing recipe')
             console.log(err)
