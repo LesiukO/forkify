8e7354a2a5a2fd0a532fe951e2adbeff0e4a6418
LesiukO <lesuksasha@gmail.com>
2019-12-29 16:13:52 +0200

    Recipe model Part-1

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 // Global app controller
 
 import Search from './models/Search'
+import Recipe from './models/Recipe'
 import * as searchView from './views/searchView'
 import { elements, renderLoader, clearLoader } from './views/base'
 
@@ -12,6 +13,9 @@ import { elements, renderLoader, clearLoader } from './views/base'
 
 const state = {}
 
+// ***********************************************
+//              SEARCH CONTROLLER  
+// ***********************************************
 const controlSearch = async () => {
     // 1. Get query from view
     const query = searchView.getInput()
@@ -49,3 +53,10 @@ elements.searchResPages.addEventListener('click', e => {
 })
 
 
+
+// ***********************************************
+//              RECIPE CONTROLLER  
+// ***********************************************
+const r = new Recipe(47746)
+r.getRecipe()
+console.log(r)
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -9,7 +9,6 @@ export default class Search {
         try {
             const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
             this.result = res.data.recipes
-            // console.log(this.result)
         }
         catch (error) {
             alert(error)
@@ -24,4 +23,3 @@ export default class Search {
 
 
 
-// const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
