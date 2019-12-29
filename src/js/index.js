bd0b830b6142e38493fad771486654016c10cc67
LesiukO <lesuksasha@gmail.com>
2019-12-30 00:03:19 +0200

    Likes Controller

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@
 import Search from './models/Search'
 import Recipe from './models/Recipe'
 import List from './models/List'
+import Likes from './models/Likes'
 import * as searchView from './views/searchView'
 import * as recipeView from './views/recipeView'
 import * as listView from './views/listView'
@@ -141,6 +142,41 @@ elements.shopping.addEventListener('click', e => {
 })
 
 
+// ***********************************************
+//              LIKE CONTROLLER  
+// ***********************************************
+const controlLike = () => {
+    if (!state.likes) state.likes = new Likes()
+    const currentID = state.recipe.id
+
+    // User has NOT yet liked current recipe
+    if (!state.likes.isLiked(currentID)) {
+        // Add like to the state
+        const newLike = state.likes.addLike(
+            currentID,
+            state.recipe.title,
+            state.recipe.author,
+            state.recipe.img,
+        )
+
+        // Toggle the like button
+
+        // Add like to UI list
+        console.log(state.likes)
+    }
+
+    // User HAS yet liked current recipe
+    else {
+        // Remove like from the state
+        state.likes.deleteLike(currentID)
+
+        // Toggle the like button
+
+        // Remove like from UI list
+        console.log(state.likes)
+    }
+}
+
 
 // Handling recipe button clicks
 elements.recipe.addEventListener('click', e => {
@@ -154,8 +190,17 @@ elements.recipe.addEventListener('click', e => {
         // Increase button is clicked
         state.recipe.updateServings('inc')
         recipeView.updateServingsIngredients(state.recipe)
-    } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+    } 
+    
+    // Add ingredients to shopping list
+    else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
         controlList()
+    }
+
+    // Handle like button
+    else if (e.target.matches('.recipe__love, .recipe__love *')) {
+        // Like controller
+        controlLike()
     }
 })
 
